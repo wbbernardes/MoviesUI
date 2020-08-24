f33bf1dbd08ab56ea8849e39de90bbb8ba81a0b5
wesley brito <wesleypkn@gmail.com>
2020-08-24 10:56:30 -0300

    Initial Commit

---
+++ MoviesUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesUI
+//
+//  Created by Wesley Brito on 24/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoviesUI/MoviesUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesUIApp.swift
+//  MoviesUI
+//
+//  Created by Wesley Brito on 24/08/20.
+//
+
+import SwiftUI
+
+@main
+struct MoviesUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
